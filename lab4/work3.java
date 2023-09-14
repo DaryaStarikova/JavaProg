aed678eda0607fb93e041e7f810522e233bbe974
DaryaStarikova <143960163+DaryaStarikova@users.noreply.github.com>
2023-09-14 13:10:14 +0300

    Add files via upload

---
+++ lab4/work3.java
@@ -0,0 +1,85 @@
+package ru.mirea.lab4;
+
+import java.util.ArrayList;
+import java.util.List;
+enum Category {
+    ELECTRONICS,
+    CLOTHING,
+    BOOKS,
+    HOME_APPLIANCES
+}
+
+class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+}
+
+class Product {
+    private String name;
+    private double price;
+    private Category category;
+
+    public Product(String name, double price, Category category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+    public double getPrice() {
+        return price;
+    }
+
+}
+
+class Catalog {
+    private List<Product> products;
+
+    public Catalog() {
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
+
+class ShoppingCart {
+    private List<Product> items;
+
+    public ShoppingCart() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(Product product) {
+        items.add(product);
+    }
+
+    public void removeItem(Product product) {
+        items.remove(product);
+    }
+
+    public List<Product> getItems() {
+        return items;
+    }
+
+    public void clearCart() {
+        items.clear();
+    }
+
+    public double calculateTotal() {
+        double total = 0;
+        for (Product item : items) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+}
