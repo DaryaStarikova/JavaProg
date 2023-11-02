abd93dcf41a088186eec9562c2f56064e0cc2d6c
DaryaStarikova <143960163+DaryaStarikova@users.noreply.github.com>
2023-11-03 00:37:11 +0300

    Add files via upload

---
+++ lab17/Task.java
@@ -0,0 +1,24 @@
+package ru.mirea.lab17;
+
+public class Task {
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+}
+
